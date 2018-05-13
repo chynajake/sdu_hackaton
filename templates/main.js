b835b6fc354d18110ed2dd3d8473cc2e3441321c
ChynaJake <chyna.jake25@gmail.com>
2018-05-13 14:41:34 +0600

    initial commit

---
+++ templates/main.js
@@ -0,0 +1,46 @@
+
+// инициализируем платформу с ID и Кодом приложения
+var platform = new H.service.Platform({
+  app_id: 'BDe5g7mw6f6WgjZX0IhZ',
+  app_code: '7_8nj2LnEkmR2JHUCRkVhw',
+  useCIT: true,
+  useHTTPS: true
+});
+
+// Получаем спислк картографических основ (спутник, вектор)
+var defaultLayers = platform.createDefaultLayers({lg:"RUS"});
+
+/* Инициализируем объект карты с аргументами:
+		контейнер где необходимо отобразить карту,
+		тип базовой карты,
+		опции
+*/
+var map = new H.Map(document.getElementById('map'),
+  defaultLayers.normal.map,{
+  	center:{
+  		"lat":43.24546,
+  		"lng":76.90902
+  	},
+  	zoom:13
+  });
+
+var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+
+// Создаем стаедартный пользовательский интерфейс
+var ui = H.ui.UI.createDefault(map, defaultLayers);
+
+// Изменение размера карты в зависимости от изменения размеров окна
+window.addEventListener('resize',function(){
+	map.getViewPort().resize()
+});
+
+
+var reader = new H.data.geojson.Reader();
+
+reader.parseData(layer);
+
+var group = new H.map.Group(); // create a group to hold map objects
+group.addObjects(reader.getParsedObjects()); // add objects to the group
+map.addObject(group); // add group to the map
+
+// group.addEventListener('tap', (ev) => {alert(ev.target.getData().properties.name)});
---
+++ templates/points.js
@@ -0,0 +1,37 @@
+var layer = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "name": "Shto to"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          43.245460000000000,
+          76.909020000000000
+        ]
+      }
+    }
+    // {
+    //   "type": "Feature",
+    //   "properties": {
+    //     "marker-color": "#7e7e7e",
+    //     "marker-size": "medium",
+    //     "marker-symbol": "",
+    //     "name": "Gde to"
+    //   },
+    //   "geometry": {
+    //     "type": "Point",
+    //     "coordinates": [
+    //       43.128703,
+    //       77.080870
+    //     ]
+    //   }
+    // }
+  ]
+}
